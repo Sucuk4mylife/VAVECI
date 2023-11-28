4ae5077fae46e9e393f49816334888995c7e2c76
Sucuk4mylife <civandenizsevin@gmail.com>
2023-11-28 06:21:27 +0100

    Co-authored-by: vecuteka <vecuteka@users.noreply.github.com>

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ function submitBooking() {
     alert("Buchung erfolgreich!");
     document.querySelector("form").reset();
 }
+
+//In js Klammer benutzen, damit es als Funktion erkannt werden kann. Alert=Benachrichtigung. Document=nachdem man gebucht hat, wird die Seite neu gestartet.//
