91c51225f3e96f1bc1e5105d5479c47f52114393
Sucuk4mylife <civandenizsevin@gmail.com>
2023-12-01 00:38:03 +0100

    update

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ function submitBooking() {
     alert("Buchung erfolgreich!");
     document.querySelector("form").reset();
 }
-
 //In js Klammer benutzen, damit es als Funktion erkannt werden kann. Alert=Benachrichtigung. Document=nachdem man gebucht hat, wird die Seite neu gestartet.//
+
+function openChat() {
+    document.getElementById("chat-container").style.display = "block";
+}
+
+
